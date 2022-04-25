2323cb61bfe3f2879a26f1c0b55cbfc00100d56e
Potato1682 <ptt1682@gmail.com>
2022-04-25 23:00:58 +0900

    feat(core.pack): loading plugins from lua/packs/*/plugins.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 require("core").setup()
 
-vim.opt.undodir = "~/.local/share/nvim/undos"
+vim.opt.undodir = "/home/ptt/.local/share/nvim/undos"
 vim.opt.undofile = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -12,6 +12,66 @@ local packer = nil
 local Pack = {}
 Pack.__index = Pack
 
+function Pack:load_plugins()
+  self.plugins = {}
+  self.rocks = {}
+
+  local get_plugin_groups = function()
+    local list = {}
+    local packs_dir = join_paths(config_dir, "lua", "packs")
+    local fd = uv.fs_scandir(packs_dir)
+
+    while true do
+      local name, typ = uv.fs_scandir_next(fd)
+
+      if not name then
+        if typ then
+          vim.notify(
+            "Cannot get plugins.\n"
+            .. typ,
+            log_levels.ERROR, {
+              title = "Pack (core)"
+            }
+          )
+        end
+
+        break
+      end
+
+      if typ ~= "directory" then
+        goto continue
+      end
+
+      local state, err = uv.fs_stat(join_paths(packs_dir, name, "plugins.lua"))
+
+      if err or state.type ~= "file" then
+        goto continue
+      end
+
+      list[#list + 1] = "packs." .. name .. ".plugins"
+
+      ::continue::
+    end
+
+    return list
+  end
+
+  local plugin_groups = get_plugin_groups()
+
+  for _, plugin_group in ipairs(plugin_groups) do
+    local plugins = reqiore(plugin_group)
+
+    for plugin, conf in pairs(plugins) do
+      self.plugins[#self.plugins + 1] = vim.tbl_extend("force", { plugin }, conf)
+    end
+  end
+
+  self.rocks = require("packs.rocks")
+end
+
+function Pack:load_plugin_manager()
+end
+
 function Pack:ensure_plugin_manager()
 end
 
