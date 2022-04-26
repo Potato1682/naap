232550c7bca3ddbaa9c9bbfe6a395fea1c983d4d
Potato1682 <ptt1682@gmail.com>
2022-04-26 23:25:29 +0900

    fix(packs/ui): add condition to work fine with termux

--- lua/packs/ui/plugins.lua
+++ lua/packs/ui/plugins.lua
@@ -2,6 +2,9 @@ local ui = {}
 local conf = require("packs.ui.config")
 
 ui["vimpostor/vim-lumen"] = {
+  cond = function()
+    return not vim.env.TERMUX_VERSION
+  end,
   setup = function()
     vim.g.lumen_startup_overwrite = 1
   end
