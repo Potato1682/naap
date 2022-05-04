467620b11a12ea3cb0b9819bf1ae2d37e496e34b
Potato1682 <ptt1682@gmail.com>
2022-05-05 08:33:35 +0900

    feat(packs/nvim-dev): keystrokes

--- lua/packs/nvim-dev/plugins.lua
+++ lua/packs/nvim-dev/plugins.lua
@@ -1,11 +1,20 @@
 local nvim_dev = {}
 
 -- nvim-dev plugins should be lazy-loaded to improve normal UX
+
 nvim_dev["dstein64/vim-startuptime"] = {
   cmd = "StartupTime",
 
   config = function()
     vim.g.startuptime_tries = 10
+  end
+}
+
+nvim_dev["tamton-aquib/keys.nvim"] = {
+  cmd = "KeysToggle",
+
+  config = function()
+    require("keys").setup {}
   end
 }
 
