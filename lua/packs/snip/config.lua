24adfb638e2a302ab49846c192d29c7be78c188e
Potato1682 <ptt1682@gmail.com>
2022-05-14 10:43:05 +0900

    fix(packs/snip): disable diagnostics when adding snippets

--- lua/packs/snip/config.lua
+++ lua/packs/snip/config.lua
@@ -3,6 +3,32 @@ local M = {}
 function M.luasnip()
   require("packs.snip.snippets")
   require("luasnip.loaders.from_vscode").lazy_load()
+
+  local luasnip = require("luasnip")
+
+  local augroup = vim.api.nvim_create_augroup("luasnip-expand", {
+    clear = true
+  })
+
+  vim.api.nvim_create_autocmd("ModeChanged", {
+    group = augroup,
+    pattern = "*:s",
+    callback = function()
+      if luasnip.in_snippet() then
+        return vim.diagnostic.disable()
+      end
+    end
+  })
+
+  vim.api.nvim_create_autocmd("ModeChanged", {
+    group = augroup,
+    pattern = "[is]:n",
+    callback = function()
+      if luasnip.in_snippet() then
+        return vim.diagnostic.enable()
+      end
+    end
+  })
 end
 
 return M
