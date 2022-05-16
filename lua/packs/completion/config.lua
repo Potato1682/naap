8131ca1608ce8f8d1df707a98859c85d8a1904e4
Potato1682 <ptt1682@gmail.com>
2022-05-16 17:03:10 +0900

    fix(packs/completion): function parens

--- lua/packs/completion/config.lua
+++ lua/packs/completion/config.lua
@@ -128,15 +128,6 @@ function M.cmp()
 
   local kind = cmp.lsp.CompletionItemKind
 
-  cmp.event:on("confirm_done", function(event)
-    local item = event.entry:get_completion_item()
-    local parensDisabled = item.data and item.data.funcParensDisabled or false
-
-    if not parensDisabled and (item.kind == kind.Method or item.kind == kind.Function) then
-      require("pairs.bracket").type_left("(")
-    end
-  end)
-
   local path_extension
   local cmd_comparators = {
     cmp.config.compare.offset,
