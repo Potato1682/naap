6532c18f695f9786dfd015e413b813ce8e667b1c
Potato1682 <ptt1682@gmail.com>
2022-05-21 16:45:16 +0900

    refactor(lsp/null-ls): split functions

--- lua/configs/lsp/null-ls.lua
+++ lua/configs/lsp/null-ls.lua
@@ -1,24 +1,34 @@
 local M = {}
 
-function M.setup()
-  local null_ls = require("null-ls")
+local null_ls = require("null-ls")
 
-  local sources = {
-    null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.code_actions.gitrebase,
-    null_ls.builtins.hover.dictionary
-  }
+-- sources with no dependencies, no install requirements
+M.builtin_sources = {
+  null_ls.builtins.code_actions.gitsigns,
+  null_ls.builtins.code_actions.gitrebase,
+  null_ls.builtins.hover.dictionary
+}
+
+function M.add_formatters()
+  local formatters = {}
 
   for _, formatter in ipairs(require("format-installer").get_installed_formatters()) do
     local config = {
       command = formatter.cmd
     }
 
-    table.insert(sources, null_ls.builtins.formatting[formatter.name].with(config))
+    table.insert(formatters, null_ls.builtins.formatting[formatter.name].with(config))
   end
 
+  return formatters
+end
+
+function M.setup()
   null_ls.setup {
-    sources = sources,
+    sources = vim.list_extend(
+      M.builtin_sources,
+      M.add_formatters()
+    ),
     on_attach = require("configs.lsp.on_attach").common_on_attach
   }
 end
