52315815b1f3623c577d6f74aba166e66b83ba96
Potato1682 <ptt1682@gmail.com>
2022-06-26 00:54:54 +0900

    feat(packs/buffer): switch buffer by last used
    You can use [b and ]b to use the previous behavior.

--- lua/packs/buffer/config.lua
+++ lua/packs/buffer/config.lua
@@ -53,21 +53,40 @@ function M.incline()
 end
 
 function M.cybu_setup()
-  local keymap = require("utils.keymap").omit("insert", "n", "<^%Tab>", { silent = true })
+  local keymap_n = require("utils.keymap").omit("prepend", "n", "b", { silent = true })
+  local keymap = require("utils.keymap").omit("insert", { "n", "v" }, "<^%Tab>", { silent = true })
 
-  keymap("", function()
+  keymap_n("[", function()
     require("cybu").cycle("next")
   end, "Next buffer")
+  keymap_n("]", function()
+    require("cybu").cycle("prev")
+  end, "Prev buffer")
+
+  keymap("", function()
+    require("cybu").cycle("next", "last_used")
+  end, "Next last used buffer")
 
   keymap("S-", function()
-    require("cybu").cycle("prev")
-  end, "Previous buffer")
+    require("cybu").cycle("prev", "last_used")
+  end, "Previous last used buffer")
 end
 
 function M.cybu()
   require("cybu").setup {
+    behavior = {
+      mode = {
+        default = {
+          view = "paging"
+        },
+        last_used = {
+          switch = "immediate",
+          view = "paging"
+        }
+      }
+    },
     style = {
-      border = "rounded"
+      border = "rounded",
     },
     exclude = require("core.constants").window.ignore_buf_change_filetypes
   }
